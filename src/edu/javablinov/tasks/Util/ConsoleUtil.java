90d064e8d94ecdca6efdfaeb512a51e30aceba61
Eugene Dubovik <dev.dubovik@gmail.com>
2022-01-03 19:10:31 +0300

    create ConsoleUtil

---
+++ src/edu/javablinov/tasks/Util/ConsoleUtil.java
@@ -0,0 +1,11 @@
+package edu.javablinov.tasks.Util;
+
+import java.util.Scanner;
+
+public class ConsoleUtil {
+    public static String getStringFromConsole(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.next();
+    }
+
+}
