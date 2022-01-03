d7fee2d760b90d594fb8c28611581ec7d3a69458
Eugene Dubovik <dev.dubovik@gmail.com>
2022-01-03 20:03:54 +0300

    added getIntFromConsole

--- src/edu/javablinov/tasks/Util/ConsoleUtil.java
+++ src/edu/javablinov/tasks/Util/ConsoleUtil.java
@@ -8,4 +8,20 @@ public class ConsoleUtil {
         return scanner.nextLine();
     }
 
+    public static int getIntFromConsole(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        int counter = 4;
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            counter--;
+            if(counter != 0) {
+                System.out.print("it`s not integer number,\n you have: " + counter + " tries -> ");
+            } else {
+                System.out.println("Sorry, try next time");
+                System.exit(1);
+            }
+        }
+        return scanner.nextInt();
+    }
+
 }
